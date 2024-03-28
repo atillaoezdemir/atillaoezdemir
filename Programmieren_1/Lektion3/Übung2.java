818718a95c9f367ed1ee37718231aa7a52befe32
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-03-28 08:35:56 +0100

    Update Übung2.java

--- Programmieren_1/Lektion3/Übung2.java
+++ Programmieren_1/Lektion3/Übung2.java
@@ -1,29 +1,29 @@
 package Lektion3;
+
 import java.util.Scanner;
+
 public class Übung2 {
-public static void main(String[] args) {
-	
-	float PI = 3.14f;
-	Scanner scanner = new Scanner(System.in);
-	System.out.print("1. pizza price: ");
-	float Pizza1p = scanner.nextFloat();
-	System.out.print("1. pizza ratio: ");
-	float Pizza1r = scanner.nextFloat();
-	
-	System.out.print("2. pizza price: ");
-	float Pizza2p = scanner.nextFloat();
-	System.out.print("2. pizza ratio: ");
-	float Pizza2r = scanner.nextFloat();
-	
-	float Erste = ((PI * ((float)Math.sqrt(Pizza1r / 2f)) / Pizza1p));
-	float Zweite = ((PI * ((float)Math.sqrt(Pizza2r / 2f)) / Pizza2p));
-	
-	if (Erste < Zweite) System.out.println("Zweite ist sinnvoll!");
-	else if (Erste == Zweite) System.out.println("Beide sind gleich!");
-	else System.out.println("Erste ist sinnvoll!");
-	
-	
-	System.out.println(Math.sqrt(4));
-	scanner.close();
+	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+        	System.out.print("1. pizza price: ");
+        	double pizza1p = scanner.nextFloat();
+        	System.out.print("1. pizza ratio: ");
+        	double pizza1r = scanner.nextFloat();
+
+        	System.out.print("2. pizza price: ");
+        	double pizza2p = scanner.nextFloat();
+        	System.out.print("2. pizza ratio: ");
+        	double pizza2r = scanner.nextFloat();
+
+        	double erste = ((Math.PI * (Math.sqrt(pizza1r / 2f)) / pizza1p));
+        	double zweite = ((Math.PI * (Math.sqrt(pizza2r / 2f)) / pizza2p));
+
+        	if (erste < zweite) System.out.println("Zweite ist sinnvoll!");
+        	else if (erste == zweite) System.out.println("Beide sind gleich!");
+        	else System.out.println("Erste ist sinnvoll!");
+
+        	scanner.close();
+    	}
 }
-}
+

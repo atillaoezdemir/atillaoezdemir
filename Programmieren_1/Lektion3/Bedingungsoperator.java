5e909490c419f4baf80b66654ea374a794efc556
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-03-29 10:13:14 +0100

    Update Bedingungsoperator.java

--- Programmieren_1/Lektion3/Bedingungsoperator.java
+++ Programmieren_1/Lektion3/Bedingungsoperator.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 
 public class Bedingungsoperator {
 	public static void main(String[] args) {
+		
 		// Variable = boolischer Ausdruck ? true Ausdruck : false Ausdruck;
 		
 		Scanner scanner = new Scanner(System.in);

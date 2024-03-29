b60e0db800dde844a474ede07df974f72f2bacdc
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-03-29 10:10:02 +0100

    Update StringCompareUndEquals.java

--- Programmieren_1/Lektion3/StringCompareUndEquals.java
+++ Programmieren_1/Lektion3/StringCompareUndEquals.java
@@ -9,11 +9,14 @@ public class StringCompareUndEquals {
 		System.out.println("Bitte geben Sie den zweiten String ein:");
 		String zweiterString = scanner.nextLine();
 		
-		if(ersterString.equals(zweiterString)) System.out.println("Diese sind gleich!");
-		else if(ersterString.compareTo(zweiterString) < 0) System.out.println("Die erste String kommt im Alphabet vor dem zweiten String!");
-		//else if(ersterString.compareTo(zweiterString) > 0) System.out.println("Die zweite String kommt im Alphabet vor dem ersten String!");
-		else System.out.println("Die zweite String kommt im Alphabet vor dem ersten String!");
-		
+		if(ersterString.equals(zweiterString)) 
+			System.out.println("Diese sind gleich!");
+		else if(ersterString.compareTo(zweiterString) < 0) 
+			System.out.println("Die erste String kommt im Alphabet vor dem zweiten String!");
+		//else if(ersterString.compareTo(zweiterString) > 0) 
+		//	System.out.println("Die zweite String kommt im Alphabet vor dem ersten String!");
+		else 
+		System.out.println("Die zweite String kommt im Alphabet vor dem ersten String!");
 		
 		scanner.close();
 		}

a0cf6c84548e476c6e3409bd350eb724ab5e30d3
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-03-31 13:08:48 +0200

    Update altKlausur_15_2.java

--- Programmieren_1/Klausurübungs/altKlausur_15_2.java
+++ Programmieren_1/Klausurübungs/altKlausur_15_2.java
@@ -1,6 +1,7 @@
 package Klausurübungs;
 
 public class altKlausur_15_2 {
+	
 	public static int[] sortiereAbsteigend(int[] array) {
 		for(int i = 0 ; i < array.length ; i++) {
 			for(int j = 0 ; j < array.length ; j++) {
@@ -14,32 +15,12 @@ public class altKlausur_15_2 {
 		}
 		return array;
 	}
+	
 	public static void main(String[] args) {
 		int[] array = { 3 , 7 , 1 , 2 , 8 };
 		int[] folge = sortiereAbsteigend(array);
-		for(int i = 0 ; i < folge.length; i++) {
+		
+		for(int i = 0 ; i < folge.length; i++)
 			System.out.println(folge[i]);
-		}
 	}
 }
-
-
-
-
-
-
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-

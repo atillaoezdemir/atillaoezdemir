326afa26470fa4203d2188c5f3a1c2ed006b8cc5
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-03-31 12:10:53 +0200

    Update Übung2.java

--- Programmieren_1/Lektion8/Übung2.java
+++ Programmieren_1/Lektion8/Übung2.java
@@ -1,7 +1,9 @@
 package Lektion8;
 
 public class Übung2 {
+	
 	public static void main(String[] args) {
+		
 		if(args.length == 2) 
 			System.out.println(Double.valueOf(args[0]) * Double.valueOf(args[0]) );
 		else 

278ebf9499bce860dd6bff20f90f287d043bd56b
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-03-31 13:13:19 +0200

    Update ss20_1.java

--- Programmieren_1/Klausurübungs/ss20_1.java
+++ Programmieren_1/Klausurübungs/ss20_1.java
@@ -1,24 +1,28 @@
 package Klausurübungs;
 
 public class ss20_1 {
+
 	public static double lnx(double x) {
 		double folge = 1;
 		double summefolge = 0;
 		double b = x - 1;
 		double bhochn = 1;
 		double y;
+	
 		for(double n = 1 ; folge > 1E-16 ; n++) {
 			y = n;
+			
 			while(y != 0) {
 				bhochn = bhochn * b;
 				y--;
 			}
-			if(n % 2 == 0) {
+			
+			if(n % 2 == 0)
 				folge = (-1 / n) * bhochn;
-			}
-			else {
+			
+			else 
 				folge = (1 / n) * bhochn;
-			}
+		
 			summefolge += folge;
 		}
 		return summefolge;

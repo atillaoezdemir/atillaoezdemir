c00afc9dff7bb437b612d8144ce4e0bb6c6d25b2
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-01 11:47:38 +0200

    Update MorseTest.java

--- Programmieren_2/klausurAufgaben/MorseTest.java
+++ Programmieren_2/klausurAufgaben/MorseTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MorseTest {
+    
     @Test
     public void testZeichenketteToMorse() {
         Morse m = new Morse();

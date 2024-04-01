d030c22fd5481c48b09414bbe401e2d9c5db9cf4
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-01 11:48:50 +0200

    Update RouletteZahlenIterator.java

--- Programmieren_2/klausurAufgaben/RouletteZahlenIterator.java
+++ Programmieren_2/klausurAufgaben/RouletteZahlenIterator.java
@@ -12,15 +12,13 @@ public class RouletteZahlenIterator implements Iterator<Integer> {
     public static void main(String[] args) {
         // b) Verwendung von RouletteZahlen
         // RouletteZahlen rouletteZahlen = new RouletteZahlen();
-        // for (Integer zahl : rouletteZahlen) {
+        // for (Integer zahl : rouletteZahlen)
         //    System.out.println(zahl);
-        // }
 
         // c) Verwendung von RouletteZahlenIterator mit for-Schleife
         RouletteZahlenIterator iterator = new RouletteZahlenIterator();
-        while (iterator.hasNext()) {
+        while (iterator.hasNext())
             System.out.println(iterator.next());
-        }
     }
 
     @Override
@@ -30,15 +28,14 @@ public class RouletteZahlenIterator implements Iterator<Integer> {
 
     @Override
     public Integer next() {
-        if (!hasNext()) {
+        if (!hasNext())
             throw new NoSuchElementException("No more elements");
-        }
 
         Random random = new Random();
         int randomNumber = random.nextInt(37); // Werte von 0 bis 36
-        if (randomNumber == 0) {
+        if (randomNumber == 0)
             terminationCounter++;
-        }
+        
         return randomNumber;
     }
 

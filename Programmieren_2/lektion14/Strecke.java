35f9ce174198ea8c18d01f2f3685f36f23f1733a
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-01 12:13:14 +0200

    Update Strecke.java

--- Programmieren_2/lektion14/Strecke.java
+++ Programmieren_2/lektion14/Strecke.java
@@ -1,32 +1,36 @@
 package de.thws.lektion14;
 
 public class Strecke {
+
     private final int anfangspunkt;
     private final int endpunkt;
-    public Strecke(int a, int b){
-        if(a < 0 || b < 0){
+    
+    public Strecke(int a, int b) {
+        if(a < 0 || b < 0)
             throw new RuntimeException("Weder anfangspunkt noch endpunkt können negativ sein!");
-        }
-        else if(a > b){
+        
+        else if(a > b)
             throw new RuntimeException("anfangspunkt kann nicht größer als endpunkt sein!");
-        }
-        else{
+        
+        else
             this.anfangspunkt = a;
             this.endpunkt = b;
-        }
     }
-    public boolean ueberschneidung(Strecke neu){
+    
+    public boolean ueberschneidung(Strecke neu) {
         return neu.endpunkt > this.anfangspunkt || neu.anfangspunkt < this.endpunkt;
     }
+    
     public String streckeToString() {
-        if (this.anfangspunkt == this.endpunkt) {
+        if (this.anfangspunkt == this.endpunkt)
             return String.valueOf(anfangspunkt);
-        }
-        else{
+        
+        else {
             String dash = "";
-            for(int i = 1; i < endpunkt - anfangspunkt ; i++) {
+            
+            for(int i = 1; i < endpunkt - anfangspunkt ; i++)
                 dash += "-";
-            }
+            
             return anfangspunkt + dash + endpunkt;
         }
     }

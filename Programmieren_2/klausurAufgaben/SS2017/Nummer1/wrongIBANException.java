0a68639713a6b72953f5d0cf8bb4cb37da7c14e9
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-01 11:35:15 +0200

    Update wrongIBANException.java

--- Programmieren_2/klausurAufgaben/SS2017/Nummer1/wrongIBANException.java
+++ Programmieren_2/klausurAufgaben/SS2017/Nummer1/wrongIBANException.java
@@ -1,6 +1,7 @@
 package de.thws.klausurAufgaben.SS2017.Nummer1;
 
 public class wrongIBANException extends Exception {
+    
     wrongIBANException() {
         super("FEHLER: Datei enthält ungültige IBAN!");
     }

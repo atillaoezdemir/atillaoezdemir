5bda73cbdd0b5c1625844046cee63247e7240369
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-01 12:38:50 +0200

    Delete Programmieren_2/lektion20 directory

--- Programmieren_2/lektion20/Farbe.java
+++
@@ -1,5 +0,0 @@
-package de.thws.lektion20;
-
-public enum Farbe {
-    HERZ, KARO, PIK, KREUZ
-}
--- Programmieren_2/lektion20/Wert.java
+++
@@ -1,5 +0,0 @@
-package de.thws.lektion20;
-
-public enum Wert {
-    ZWEI, DREI, VIER, FUENF, SECHS, SIEBEN, ACHT, NEUN, DAME, KOENIG, ZEHN, ASS , BUBE;
-}

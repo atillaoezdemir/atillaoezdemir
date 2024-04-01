94ce42b0456e024a35e079ac1cfa8a33e60441d3
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-01 11:43:01 +0200

    Update Message.java

--- Programmieren_2/klausurAufgaben/Twotter/Message.java
+++ Programmieren_2/klausurAufgaben/Twotter/Message.java
@@ -3,6 +3,7 @@ package de.thws.klausurAufgaben.Twotter;
 import java.io.Serializable;
 
 public class Message implements Serializable {
+    
     private final String content;
 
     public Message(String content) {

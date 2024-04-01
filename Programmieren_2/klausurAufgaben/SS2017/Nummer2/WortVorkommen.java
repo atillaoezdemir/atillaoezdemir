d671a80bc977b79dd2a605e92a06786917d20684
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-01 11:39:30 +0200

    Update WortVorkommen.java

--- Programmieren_2/klausurAufgaben/SS2017/Nummer2/WortVorkommen.java
+++ Programmieren_2/klausurAufgaben/SS2017/Nummer2/WortVorkommen.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 public class WortVorkommen {
+    
     private final Map<Wort, List<Position>> wortPositionen;
 
     public WortVorkommen() {
@@ -14,26 +15,29 @@ public class WortVorkommen {
 
     public void einfuegen(Wort wort, Position position) {
         List<Position> positionen = wortPositionen.get(wort);
-        if (positionen == null) {
+        
+        if (positionen == null)
             positionen = new ArrayList<>();
-        }
+        
         positionen.add(position);
         wortPositionen.put(wort, positionen);
     }
 
     public List<Position> holeAlle(Wort wort) {
         List<Position> positionen = wortPositionen.get(wort);
-        if (positionen.size() == 0) {
+        
+        if (positionen.size() == 0)
             return new ArrayList<>();
-        }
+        
         return positionen;
     }
 
     public Position holeLetztePosition(Wort wort) {
         List<Position> positionen = wortPositionen.get(wort);
-        if (positionen.size() != 0) {
+        
+        if (positionen.size() != 0)
             return positionen.get(positionen.size() - 1);
-        }
+    
         return null;
     }
 

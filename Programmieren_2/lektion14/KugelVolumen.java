9620c14c6e8fcf3d5c405617931a8b1c1c40fb04
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-01 12:09:48 +0200

    Update KugelVolumen.java

--- Programmieren_2/lektion14/KugelVolumen.java
+++ Programmieren_2/lektion14/KugelVolumen.java
@@ -1,11 +1,11 @@
 package de.thws.lektion14;
 
 public class KugelVolumen {
-    public static double berechneKugelvolumen(double radius)
-    {
-       if (radius < 0) {
+    
+    public static double berechneKugelvolumen(double radius) {
+       if (radius < 0)
             throw new RuntimeException("Radius muss positiv sein!");
-        }
+
         return  Math.PI * radius * radius * radius * 4.0 / 3.0 ;
     }
 }

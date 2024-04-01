e194ad94ebec6cc6be1a69f67e3674f0b1512f94
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-01 12:34:14 +0200

    Delete Programmieren_2/lektion16/muenzautomat directory

--- Programmieren_2/lektion16/muenzautomat/ChangeCalculator.java
+++
@@ -1,6 +0,0 @@
-package de.thws.lektion16.muenzautomat;
-
-public interface ChangeCalculator
-{
-  public int[] getChange(int euros, int cent);
-}
--- Programmieren_2/lektion16/muenzautomat/Main.java
+++
@@ -1,9 +0,0 @@
-package de.thws.lektion16.muenzautomat;
-
-public class Main {
-    public static void main(String[] args){
-        int[] a = AdvancedChangeCalculator.getChange(3, 88);
-        for (int j : a) System.out.println(j);
-
-    }
-}

822ae9c97fc83b686650dd19062ac7acce557269
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-01 12:15:01 +0200

    Update StreckeTest.java

--- Programmieren_2/lektion14/StreckeTest.java
+++ Programmieren_2/lektion14/StreckeTest.java
@@ -1,33 +1,38 @@
 package de.thws.lektion14;
+
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class StreckeTest {
+
     @Test
     public void variablenTest(){
         try {
             new Strecke(4,1);
             fail("Runtime Exception erwartet!");
-        }catch (RuntimeException e){
+        }catch (RuntimeException e) {
             String errorMessage = e.getMessage();
             assertEquals("anfangspunkt kann nicht größer als endpunkt sein!", errorMessage);
         }
     }
+    
     @Test
-    public void negativeVariablenTest(){
-        try{
+    public void negativeVariablenTest() {
+        try {
             new Strecke(-1,-5);
         }catch(RuntimeException e){
             String errorMessagge = e.getMessage();
             assertEquals("Weder anfangspunkt noch endpunkt können negativ sein!", errorMessagge);
         }
     }
+    
     @Test
     public void ueberschneidungTest(){
         new Strecke(2,8);
         Strecke neu = new Strecke(5,40);
         assertTrue(neu.ueberschneidung(neu));
     }
+    
     @Test
     public void stringMethodeTest(){
         Strecke neu = new Strecke(1,6);

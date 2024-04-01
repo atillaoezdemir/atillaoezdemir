361886971cad29e7873d693b5c82f29cc9440468
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-01 11:45:20 +0200

    Update User.java

--- Programmieren_2/klausurAufgaben/Twotter/User.java
+++ Programmieren_2/klausurAufgaben/Twotter/User.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class User implements Serializable {
+    
     static List<User> users = new ArrayList<>();
     private final String benutzername;
     private final String passwort;
@@ -14,16 +15,14 @@ public class User implements Serializable {
             this.benutzername = benutzername;
             this.passwort = passwort;
             users.add(this);
-        } else {
+        } else
             throw new RuntimeException("Dieser Benutzername ist schon benutzt!");
-        }
     }
 
     private boolean isUserInList(String username) {
         for (User user : users) {
-            if (user.benutzername.equals(username)) {
+            if (user.benutzername.equals(username))
                 return true;
-            }
         }
         return false;
     }

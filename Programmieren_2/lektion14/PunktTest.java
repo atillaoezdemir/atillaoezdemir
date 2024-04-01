f1547528ff031f7540eb01cab5334674f76e0457
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-01 12:11:50 +0200

    Update PunktTest.java

--- Programmieren_2/lektion14/PunktTest.java
+++ Programmieren_2/lektion14/PunktTest.java
@@ -1,8 +1,10 @@
 package de.thws.lektion14;
+
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 public class PunktTest {
+
     @Test
     public void testGueltigeKoordinaten() {
         Punkt p = new Punkt();
@@ -10,58 +12,52 @@ public class PunktTest {
         assertEquals(100, p.x);
         assertEquals(200, p.y);
     }
+    
     @Test
     public void testNegativeX() {
         Punkt p = new Punkt();
-            try
-            {
+            try {
                 p.verschiebePunkt(-100, 200);
                 fail("Runtime Exception erwartet");
             }
-            catch(RuntimeException e)
-            {
+            catch(RuntimeException e) {
                 String errorMessage = e.getMessage();
                 assertEquals("Ungültige Zahlen!", errorMessage);
             }
-    }
+    }    
+    
     @Test
     public void testNegativeY() {
         Punkt p = new Punkt();
-        try
-        {
+        try {
             p.verschiebePunkt(-100, 200);
             fail("Runtime Exception erwartet");
         }
-        catch(RuntimeException e)
-        {
+        catch(RuntimeException e) {
             String errorMessage = e.getMessage();
             assertEquals("Ungültige Zahlen!", errorMessage);
         }
     }
+    
     @Test
     public void testGrosseX() {
         Punkt p = new Punkt();
-        try
-        {
+        try {
             p.verschiebePunkt(1921, 200);
             fail("Runtime Exception erwartet");
-        }
-        catch(RuntimeException e)
-        {
+        } catch(RuntimeException e) {
             String errorMessage = e.getMessage();
             assertEquals("Ungültige Zahlen!", errorMessage);
         }
     }
+    
     @Test
     public void testGrosseY() {
         Punkt p = new Punkt();
-        try
-        {
+        try {
             p.verschiebePunkt(100, 1081);
             fail("Runtime Exception erwartet");
-        }
-        catch(RuntimeException e)
-        {
+        } catch(RuntimeException e) {
             String errorMessage = e.getMessage();
             assertEquals("Ungültige Zahlen!", errorMessage);
         }

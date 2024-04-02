12a875ab8cbafee526780252f31ba9a8d237da1c
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-02 13:22:43 +0200

    Update Umsatzberechner.java

--- Programmieren_2/klausurAufgaben/Mietwagen/Umsatzberechner.java
+++ Programmieren_2/klausurAufgaben/Mietwagen/Umsatzberechner.java
@@ -8,9 +8,9 @@ public class Umsatzberechner {
     public double gesamtUmsatz() {
         double wert = 0;
 
-        for (int i = 0; i < fahrzeuge.size(); i++) {
+        for (int i = 0; i < fahrzeuge.size(); i++)
             wert += fahrzeuge.get(i).berechneGeld(fahrtenbuch.get(fahrzeuge.get(i)));
-        }
+        
         return wert;
     }
 
@@ -21,6 +21,7 @@ public class Umsatzberechner {
             if (fahrzeuge.get(i) instanceof ElektroFahrzeug)
                 wert += fahrzeuge.get(i).berechneGeld(fahrtenbuch.get(fahrzeuge.get(i)));
         }
+        
         return wert;
     }
 }

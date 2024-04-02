571200648b186d08a47bbab13044c01ef3dc7b39
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-02 14:33:11 +0200

    Update vererbung.java

--- Programmieren_2/lektion22aa/uebung1/vererbung.java
+++ Programmieren_2/lektion22aa/uebung1/vererbung.java
@@ -1,10 +1,13 @@
 package de.thws.lektion22aa.uebung1;
 
 public class vererbung extends Thread {
+
     private String msg;
+    
     public vererbung(String msg) {
         this.msg = msg;
     }
+    
     public void run() {
         while (true) {
             System.out.println(msg);
@@ -15,6 +18,7 @@ public class vererbung extends Thread {
             }
         }
     }
+    
     public static void main(String[] args) {
         vererbung yangThread = new vererbung("Yang");
         vererbung yingThread = new vererbung("Ying");

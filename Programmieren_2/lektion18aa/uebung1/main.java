4ab9b19a7a9f8e736a92740f20577bd58fdc1470
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-02 14:10:17 +0200

    Update main.java

--- Programmieren_2/lektion18aa/uebung1/main.java
+++ Programmieren_2/lektion18aa/uebung1/main.java
@@ -2,16 +2,21 @@ package de.thws.lektion18aa.uebung1;
 import java.io.*;
 
 public class main {
+
     static void splitStudiengaenge(String path) throws IOException, matrikelNummerException{
         try (BufferedReader br = new BufferedReader(new FileReader(path));
              BufferedWriter inf = new BufferedWriter(new FileWriter("inf.txt"));
              BufferedWriter ec = new BufferedWriter(new FileWriter("ec.txt") );
              BufferedWriter winf = new BufferedWriter(new FileWriter("winf.txt"))) {
+    
             do {
                 String line = br.readLine();
-                if (line == null || line.isEmpty()) break;
+                if (line == null || line.isEmpty()) 
+                    break;
+                
                 try {
                     int matrikelNummer = Integer.parseInt(line);
+                
                     if (matrikelNummer >= 5000000 && Integer.parseInt(line) <= 5099999) {
                         winf.write(line);
                         winf.newLine();
@@ -21,7 +26,8 @@ public class main {
                     } else if (matrikelNummer >= 6100000 && Integer.parseInt(line) <= 6199999) {
                         ec.write(line);
                         ec.newLine();
-                    } else throw new matrikelNummerException("Ungültige Matrikelnummer: " + line);
+                    } else
+                        throw new matrikelNummerException("Ungültige Matrikelnummer: " + line);
                 } catch (NumberFormatException e) {
                     System.err.println("NumberFormatException: " + e.getMessage());
                 }
@@ -30,6 +36,7 @@ public class main {
             e.printStackTrace();
         }
     }
+ 
     public static void main(String[] args) {
         try {
             splitStudiengaenge("C:\\Users\\Ati\\IdeaProjects\\programmieren2\\nums.txt");

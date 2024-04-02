4c7c3d10c3a70cc44a4315b819ac5f69603cdcc8
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-02 13:50:43 +0200

    Update Feld.java

--- Programmieren_2/lektion16/schach/Feld.java
+++ Programmieren_2/lektion16/schach/Feld.java
@@ -1,33 +1,28 @@
 package de.thws.lektion16.schach;
 
-public class Feld
-{
+public class Feld {
+  
   int x;
   int y;
   
-  public Feld(int x, int y)
-  {
+  public Feld(int x, int y) {
     this.x = x;
     this.y = y;
   }
 
-  public int getX()
-  {
+  public int getX() {
     return x;
   }
 
-  public void setX(int x)
-  {
+  public void setX(int x) {
     this.x = x;
   }
 
-  public int getY()
-  {
+  public int getY() {
     return y;
   }
 
-  public void setY(int y)
-  {
+  public void setY(int y) {
     this.y = y;
   }
 }

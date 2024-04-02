ecf88ef10626f1cd720fc87026b1ef06811c4aeb
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-02 13:50:11 +0200

    Update Dame.java

--- Programmieren_2/lektion16/schach/Dame.java
+++ Programmieren_2/lektion16/schach/Dame.java
@@ -1,28 +1,25 @@
 package de.thws.lektion16.schach;
 
-public class Dame extends AbstractFigur implements Laeufer, Turm
-{
+public class Dame extends AbstractFigur implements Laeufer, Turm {
+  
   Laeufer laeufer;
   Turm turm;
   
-  public Dame(int x, int y)
-  {
+  public Dame(int x, int y) {
     super(x, y);
     laeufer = new LaeuferImpl(x, y);
     turm = new TurmImpl(x, y);
   }
 
   @Override
-  public Brett gibErlaubteFelder()
-  {
+  public Brett gibErlaubteFelder() {
     Brett brettLaeufer = laeufer.gibErlaubteFelder();
     Brett brettTurm = turm.gibErlaubteFelder();
     Brett kombiniertesBrett = brettTurm.kombiniere(brettLaeufer);
     return kombiniertesBrett;
   }
 
-  public static void main(String[] args)
-  {
+  public static void main(String[] args) {
     Dame d = new Dame(4,5);
     System.out.println(d.gibErlaubteFelder());
     Brett brett = d.gibErlaubteFelder();
@@ -30,8 +27,11 @@ public class Dame extends AbstractFigur implements Laeufer, Turm
     {
       for (int i = 1; i <= 8; i++)
       {
-        if (brett.gibFeld(i, j)) System.out.print("x");
-        else System.out.print("o");
+        if (brett.gibFeld(i, j)) 
+          System.out.print("x");
+        
+        else 
+          System.out.print("o");
       }
       System.out.println();
     }

e907ffd130e60e18692d17e1f124a3ad38ef481c
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-02 13:22:16 +0200

    Update Kleintransporter.java

--- Programmieren_2/klausurAufgaben/Mietwagen/Kleintransporter.java
+++ Programmieren_2/klausurAufgaben/Mietwagen/Kleintransporter.java
@@ -10,6 +10,7 @@ public class Kleintransporter extends Fahrzeug {
     public double berechneGeld(Fahrt f) {
         if (f.min <= 60)
             return 0.3 * f.min;
+            
         else
             return 25 * (f.min - 60) + 30 * f.km;
     }

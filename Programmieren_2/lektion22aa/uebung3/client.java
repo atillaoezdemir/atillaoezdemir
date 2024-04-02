0092a69dd905344b64ce2e721a137be17b04f994
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-02 14:34:14 +0200

    Update client.java

--- Programmieren_2/lektion22aa/uebung3/client.java
+++ Programmieren_2/lektion22aa/uebung3/client.java
@@ -19,13 +19,15 @@ public class client {
         try (Socket socket = new Socket("localhost", 8080);
              BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
              BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+            
             writer.write("1, 2, 3, 4, 5, 6, 7, 8");
             writer.flush();
 
             String output;
-            while ((output = reader.readLine()) != null) {
+            
+            while ((output = reader.readLine()) != null)
                 System.out.println(output);
-            }
+            
         } catch (IOException e) {
             throw new RuntimeException(e);
         }

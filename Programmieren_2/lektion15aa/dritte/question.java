914ee2f4c0d8d82f4cc3355e5b8e1986dac5d1f7
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-02 13:36:55 +0200

    Update question.java

--- Programmieren_2/lektion15aa/dritte/question.java
+++ Programmieren_2/lektion15aa/dritte/question.java
@@ -1,12 +1,15 @@
 package de.thws.lektion15aa.dritte;
 
 public abstract class question {
+    
     String question;
     String antwort;
+    
     public question(String question, String antwort){
         this.question = question;
         this.antwort = antwort;
     }
+    
     abstract void askQuestion();
 
     abstract boolean checkAnswer(String antwort);

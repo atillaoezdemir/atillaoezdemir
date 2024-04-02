1a69cf8d61f7acc7d4efaba6f5c22d1328a10e77
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-02 14:02:39 +0200

    Update stackVererbung.java

--- Programmieren_2/lektion16aa/stack/stackVererbung.java
+++ Programmieren_2/lektion16aa/stack/stackVererbung.java
@@ -3,14 +3,16 @@ package de.thws.lektion16aa.stack;
 import java.util.ArrayList;
 
 public class stackVererbung extends ArrayList<Object> {
+
     public void push(Object a){
         add(a);
     }
+    
     public Object pop(){
-        if (size() > 0) {
+        if (size() > 0)
             return remove(size() - 1);
-        } else {
+        
+        else
             throw new RuntimeException("Stack is empty!");
-        }
     }
 }

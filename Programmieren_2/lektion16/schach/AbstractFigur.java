a5ca3b9f738d53d96a3376145c12899782fa280f
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-02 13:46:22 +0200

    Update AbstractFigur.java

--- Programmieren_2/lektion16/schach/AbstractFigur.java
+++ Programmieren_2/lektion16/schach/AbstractFigur.java
@@ -1,37 +1,34 @@
 package de.thws.lektion16.schach;
 
-public abstract class AbstractFigur implements Figur
-{
+public abstract class AbstractFigur implements Figur {
+  
   int x;
   int y;
   
-  public AbstractFigur(int x, int y)
-  {
+  public AbstractFigur(int x, int y) {
     setX(x);
     setY(y);
   }
 
-  public int getX()
-  {
+  public int getX() {
     return x;
   }
 
-  public final void setX(int x)
-  {
+  public final void setX(int x) {
     if (x >= 1 && x <= 8)
       this.x = x;
-    else throw new RuntimeException("Es muss 1 <= x <= 8 sein!");
+    else
+      throw new RuntimeException("Es muss 1 <= x <= 8 sein!");
   }
 
-  public int getY()
-  {
+  public int getY() {
     return y;
   }
 
-  public void setY(int y)
-  {
+  public void setY(int y) {
     if (y >= 1 && y <= 8)
       this.y = y;
-    else throw new RuntimeException("Es muss 1 <= y <= 8 sein!");
+    else 
+      throw new RuntimeException("Es muss 1 <= y <= 8 sein!");
   }
 }

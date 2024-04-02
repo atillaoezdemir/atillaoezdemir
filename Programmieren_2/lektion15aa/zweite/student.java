d3eee09bde3a850e4d90926bc5c09ad217a1dcce
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-02 13:42:13 +0200

    Update student.java

--- Programmieren_2/lektion15aa/zweite/student.java
+++ Programmieren_2/lektion15aa/zweite/student.java
@@ -1,9 +1,11 @@
 package de.thws.lektion15aa.zweite;
 
-public class student extends person{
+public class student extends person {
+    
     public student(String fach){
         super(fach);
     }
+    
     public void gibTaetigkeitAus(){
         System.out.println("Der Student besucht das Fach " + this.fach);
     }

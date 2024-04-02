65414123cd9ddd8acab9373a793b28c4b9a1201c
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-02 14:28:22 +0200

    Update Karte.java

--- Programmieren_2/lektion20aa/uebung2/Karte.java
+++ Programmieren_2/lektion20aa/uebung2/Karte.java
@@ -5,18 +5,19 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Karte implements Comparable<Karte> {
+    
     Farbe farbe;
     Wert wert;
+    
     public Karte(Farbe farbe, Wert wert) {
         this.farbe = farbe;
         this.wert = wert;
     }
+    
     public int compareTo(Karte other) {
         List<Karte> reihenfolgeBube = Arrays.asList(new Karte(Farbe.KREUZ, Wert.BUBE), new Karte(Farbe.PIK, Wert.BUBE), new Karte(Farbe.HERZ, Wert.BUBE), new Karte(Farbe.KARO, Wert.BUBE));
         List<Farbe> reihenfolgeFarbe = Arrays.asList(Farbe.KREUZ, Farbe.PIK, Farbe.HERZ, Farbe.KARO);
         List<Wert> reihenfolgeWert = Arrays.asList(Wert.ASS, Wert.ZEHN, Wert.KOENIG, Wert.DAME, Wert.NEUN, Wert.ACHT, Wert.SIEBEN);
-        //belirli öğelerdle liste oluşturmanın kısa yolu bu şekilde
-        //diğer türlü add ile tek tek eklemek gerekir
 
         int karteIndex1 = reihenfolgeBube.indexOf(this);
         int karteIndex2 = reihenfolgeBube.indexOf(other);
@@ -25,12 +26,11 @@ public class Karte implements Comparable<Karte> {
         int wertIndex1 = reihenfolgeWert.indexOf(this.wert);
         int wertIndex2 = reihenfolgeWert.indexOf(other.wert);
 
-        if (karteIndex1 != karteIndex2) {
+        if (karteIndex1 != karteIndex2) 
             return Integer.compare(karteIndex1, karteIndex2);
-        } else if (farbeIndex1 != farbeIndex2) {
+        else if (farbeIndex1 != farbeIndex2)
             return Integer.compare(farbeIndex1, farbeIndex2);
-        } else {
+        else
             return Integer.compare(wertIndex1, wertIndex2);
-        }
     }
 }

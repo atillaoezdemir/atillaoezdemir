e62f7410aa7732e806e837997df031b427d74da5
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-02 14:15:15 +0200

    Update HangmanGuess.java

--- Programmieren_2/lektion19aa/uebung1/HangmanGuess.java
+++ Programmieren_2/lektion19aa/uebung1/HangmanGuess.java
@@ -2,6 +2,7 @@ package de.thws.lektion19aa.uebung1;
 import java.io.Serializable;
 
 public class HangmanGuess implements Serializable {
+    
     private char letter;
 
     public HangmanGuess(char letter) {

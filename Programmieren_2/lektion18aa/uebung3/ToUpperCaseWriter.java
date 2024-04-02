2ae14cdec320fa6bc4a2fc82417723b9dbbd52ec
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-02 14:12:04 +0200

    Update ToUpperCaseWriter.java

--- Programmieren_2/lektion18aa/uebung3/ToUpperCaseWriter.java
+++ Programmieren_2/lektion18aa/uebung3/ToUpperCaseWriter.java
@@ -4,19 +4,18 @@ import java.io.IOException;
 import java.io.Writer;
 
 public class ToUpperCaseWriter extends Writer {
+
         Writer writer;
-        public ToUpperCaseWriter(Writer writer)
-        {
+        
+        public ToUpperCaseWriter(Writer writer) {
             this.writer = writer;
         }
 
-        public void write(char c) throws IOException
-        {
+        public void write(char c) throws IOException {
             writer.write(Character.toUpperCase(c));
         }
 
-        public void write(char[] cbuf, int off, int len) throws IOException
-        {
+        public void write(char[] cbuf, int off, int len) throws IOException {
             for (int i = off; i < off + len; i++)
                 write(cbuf[i]);
         }

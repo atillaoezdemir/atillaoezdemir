2c877cc372fb1f44f41d60db59446a33c23061a1
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-02 13:47:06 +0200

    Update Brett.java

--- Programmieren_2/lektion16/schach/Brett.java
+++ Programmieren_2/lektion16/schach/Brett.java
@@ -1,14 +1,14 @@
 package de.thws.lektion16.schach;
 
-public class Brett
-{
+public class Brett {
+  
   boolean[][] brett = new boolean[8][8];
 
-  public Brett()
-  {
-
+  public Brett() {
+  
   }
-  public Brett kombiniere(Brett b){
+  
+  public Brett kombiniere(Brett b) {
     for(int i = 0; i <= 8; i++){
       for(int j = 0; j <= 8; j++){
         if(b.brett[i][j] == true){
@@ -18,15 +18,13 @@ public class Brett
     }
     return this;
   }
-  public void markiereFeld(int x, int y)
-  {
+  
+  public void markiereFeld(int x, int y) {
     if(y >= 1 && y <= 8 && x >= 1 && x <= 8)
       brett[x-1][y-1] = true;
   }
   
-  public boolean gibFeld(int x, int y)
-  {
+  public boolean gibFeld(int x, int y) {
     return brett[x-1][y-1];
   }
-  
 }

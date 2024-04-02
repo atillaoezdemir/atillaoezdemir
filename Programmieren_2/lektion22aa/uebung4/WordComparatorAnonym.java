99cc9b543b457a8dc9b39a7b68897cdc2fe5185d
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-02 14:35:39 +0200

    Update WordComparatorAnonym.java

--- Programmieren_2/lektion22aa/uebung4/WordComparatorAnonym.java
+++ Programmieren_2/lektion22aa/uebung4/WordComparatorAnonym.java
@@ -1,7 +1,9 @@
 package de.thws.lektion22aa.uebung4;
+
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.TreeSet;
+
 public class WordComparatorAnonym {
     public static void main(String[] args) {
         TreeSet<String> t = new TreeSet<>(new Comparator<>() {
@@ -13,6 +15,7 @@ public class WordComparatorAnonym {
 
         String[] alleWoerter = {"Barriere", "Buch", "Bibel", "Beige", "Bienenschwarm", "Bein", "Beil", "Christ", "Christian", "Carmen"};
         t.addAll(Arrays.asList(alleWoerter));
+
         for (String wort : t)
             System.out.println(wort);
     }

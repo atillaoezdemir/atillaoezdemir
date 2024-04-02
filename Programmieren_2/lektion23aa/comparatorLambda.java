e0f0ab52124fa1c2b5f7ceccdb1d245e934d24a9
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-02 14:37:23 +0200

    Update comparatorLambda.java

--- Programmieren_2/lektion23aa/comparatorLambda.java
+++ Programmieren_2/lektion23aa/comparatorLambda.java
@@ -1,11 +1,15 @@
 package de.thws.lektion23aa;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+
 public class comparatorLambda {
+
     public static void main(String[] args) {
         List<String> strings = new ArrayList<>();
+    
         strings.add("Bannannaaa");
         strings.add("Apple");
         strings.add("Orange");

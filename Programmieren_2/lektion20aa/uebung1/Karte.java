80a597a440063445e72b0e4b41d5b8d774638604
Atilla Özdemir <164641796+atillaoezdemir@users.noreply.github.com>
2024-04-02 14:24:58 +0200

    Update Karte.java

--- Programmieren_2/lektion20aa/uebung1/Karte.java
+++ Programmieren_2/lektion20aa/uebung1/Karte.java
@@ -1,13 +1,18 @@
 package de.thws.lektion20aa.uebung1;
+
 import java.util.Arrays;
 import java.util.List;
+
 public class Karte implements Comparable<Karte>{
+
     Farbe farbe;
     Wert wert;
+    
     public Karte(Farbe f, Wert w) {
         this.farbe = f;
         this.wert = w;
     }
+    
     @Override
     public int compareTo(Karte other) {
         List<Wert> wertReihenfolge = Arrays.asList(Wert.BUBE, Wert.ASS, Wert.ZEHN, Wert.KOENIG, Wert.DAME, Wert.NEUN, Wert.ACHT, Wert.SIEBEN, Wert.SIEBEN, Wert.SECHS, Wert.FUENF, Wert.VIER, Wert.DREI, Wert.ZWEI);
@@ -18,12 +23,12 @@ public class Karte implements Comparable<Karte>{
         int farbeIndex1 = farbeReihenfolge.indexOf(this.farbe);
         int farbeIndex2 = farbeReihenfolge.indexOf(other.farbe);
 
-        if (wertIndex1 != wertIndex2) {
+        if (wertIndex1 != wertIndex2)
             return Integer.compare(wertIndex1, wertIndex2);
-        } else {
+        else
             return Integer.compare(farbeIndex1, farbeIndex2);
-        }
     }
+    
     @Override
     public String toString() {
         return farbe + " " + wert;

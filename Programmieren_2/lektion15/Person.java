b4483a2d41dc4e58f22475cb66871be8e0d1d4f1
Atilla Özdemir <atillaoezdemir132@gmail.com>
2024-04-03 14:52:15 +0200

    Folder name is changed

--- Programmieren_2/lektion15/Person.java
+++
@@ -1,9 +0,0 @@
-package de.thws.lektion15;
-
-public abstract class Person {
-    protected String fach;
-    public Person(String fach){
-        this.fach = fach;
-    }
-    abstract void gibTaetigkeitAus();
-}
